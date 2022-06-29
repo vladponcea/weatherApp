5578a304606d189c2442b157cf4ad232b897a76a
vladponcea <vladponcea@icloud.com>
2022-06-29 15:11:57 +0300

    first push

---
+++ weatherApp/weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Vladut Mihai Poncea on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
